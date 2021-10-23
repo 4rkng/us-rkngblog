a1b058556f1090ad6e624a16fa51488602f87c1a
Hiragi Rikunagi <4rkng@protonmail.com>
2021-10-23 09:30:05 +0900

    1st commit

---
+++ loop-templates/content-single.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Single post partial template.
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+?>
+
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+	<header class="entry-header">
+
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+		<div class="entry-meta">
+			<!--投稿日/投稿者の表示-->
+			<span class="posted-on">投稿日:<?php  the_time(get_option('date_format'));?></span>
+			<san class="get-Author">投稿者:<?php the_author();?></span>
+
+			<!-- ?php understrap_posted_on(); ?-->
+
+		</div><!-- .entry-meta -->
+
+	</header><!-- .entry-header -->
+
+	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+
+	<div class="entry-content">
+
+		<?php the_content(); ?>
+
+		<?php
+		wp_link_pages(
+			array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+				'after'  => '</div>',
+			)
+		);
+		?>
+
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+
+		<?php understrap_entry_footer(); ?>
+
+	</footer><!-- .entry-footer -->
+
+</article><!-- #post-## -->
---
+++ loop-templates/rk-content.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Post rendering content according to caller of get_template_part.
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+?>
+
+<article <?php post_class("col-md-5 shadow post_margin fadein_article"); ?> id="post-<?php the_ID(); ?>">
+
+	<header class="entry-header">
+
+		<?php
+		the_title(
+			sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
+			'</a></h2>'
+		);
+		?>
+
+		<?php if ( 'post' == get_posts() ) : ?>
+
+			<div class="entry-meta">
+				<!--投稿日/投稿者の表示-->
+			<span class="posted-on">投稿日:<?php  the_time(get_option('date_format'));?></span>
+			<san class="get-Author">投稿者:<?php the_author();?></span>
+
+			<!-- ?php understrap_posted_on(); ?-->
+			</div><!-- .entry-meta -->
+
+		<?php endif; ?>
+
+	</header><!-- .entry-header -->
+
+	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+
+	<div class="entry-content">
+
+		<?php the_excerpt(); ?>
+
+		<?php
+		wp_link_pages(
+			array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+				'after'  => '</div>',
+			)
+		);
+		?>
+
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+
+		<?php understrap_entry_footer(); ?>
+
+	</footer><!-- .entry-footer -->
+
+</article><!-- #post-## -->
